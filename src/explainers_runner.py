968762ed73b434716dbffb16e503944bdb25bd2e
sapkotaruz11 <sapkotarupesh15@gmail.com>
2024-04-09 10:53:18 +0200

    update RGAT

--- src/explainers_runner.py
+++ src/explainers_runner.py
@@ -1,6 +1,7 @@
 from src.Explainer import Explainer
 import json
 import os
+import pandas as pd
 
 
 def run_explainers(
@@ -9,7 +10,7 @@ def run_explainers(
     print(f"Running explainers for {no_of_runs} runs for dataset {dataset}")
     performances = {}
     preds = {}
-    predictions_data = {}
+    predictions_dfs = []
 
     for i in range(no_of_runs):
         print(f"Starting the  Run", i + 1)
@@ -29,8 +30,8 @@ def run_explainers(
             elif explainer == "CELOE":
                 performances[i][explainer] = my_explainer.evaluations.get(explainer, {})
                 preds[i][explainer] = my_explainer.explanations.get(explainer, {})
-
-        predictions_data[i] = my_explainer.pred_df.to_json()
+        my_explainer.pred_df["Run"] = int(i) + 1
+        predictions_dfs.append(my_explainer.pred_df)
 
     for explainer in explainers:
         file_path_evaluations = (
@@ -62,61 +63,10 @@ def run_explainers(
                     json_file,
                     indent=2,
                 )
-
-    file_path_predictions_df = f"results/predictions/{model}/{dataset}.json"
+    big_df = pd.concat(predictions_dfs, ignore_index=True)
+    file_path_predictions_df = f"results/predictions/{model}/{dataset}.csv"
     directory = os.path.dirname(file_path_predictions_df)
     # Create the directory if it doesn't exist
     if not os.path.exists(directory):
         os.makedirs(directory)
-    with open(file_path_predictions_df, "w") as json_file:
-        json.dump(predictions_data, json_file, indent=2)
-
-
-# def run_explainers(dataset, explainers, print_explainer_loss=True, no_of_runs=5):
-#     print(f"Running explainers  for {no_of_runs} runs. for dataset {dataset} ")
-#     pg_performance = {}
-#     sgx_performance = {}
-#     evo_performance = {}
-#     celoe_performance = {}
-#     evo_preds = {}
-#     celoe_preds = {}
-#     predicition_data = {}
-
-#     for i in range(no_of_runs):
-#         my_explainer = Explainer(explainers=explainers, dataset="mutag")
-#         evo_performance[i] = my_explainer.evaluations["EvoLearner"]
-#         pg_performance[i] = my_explainer.evaluations["PGExplainer"]
-#         celoe_performance[i] = my_explainer.evaluations["CELOE"]
-#         sgx_performance[i] = my_explainer.evaluations["PGExplainer"]
-#         evo_preds[i] = my_explainer.explanations["EvoLearner"]
-#         celoe_preds[i] = my_explainer.explanations["CELOE"]
-#         predicition_data[i] = my_explainer.pred_df.to_json()
-
-#     file_path_evaluations_pg = f"results/evaluations/PGExplainer/{dataset}.json"
-#     with open(file_path_evaluations_pg, "w") as json_file:
-#         json.dump(pg_performance, json_file, indent=2)
-
-#     file_path_evaluations_sgx = f"results/evaluations/SubGraphX/{dataset}.json"
-#     with open(file_path_evaluations_sgx, "w") as json_file:
-#         json.dump(sgx_performance, json_file, indent=2)
-
-#     file_path_predictions_evo = f"results/predictions/EVO/{dataset}.json"
-#     file_path_evaluations_evo = f"results/evaluations/EVO/{dataset}.json"
-
-#     with open(file_path_predictions_evo, "w") as json_file:
-#         json.dump(evo_preds, json_file, indent=2)
-
-#     with open(file_path_evaluations_evo, "w") as json_file:
-#         json.dump(evo_performance, json_file, indent=2)
-
-#     file_path_predictions_celoe = f"results/predictions/CELOE/{dataset}.json"
-#     file_path_evaluations_celoe = f"results/evaluations/CELOE/{dataset}.json"
-
-#     with open(file_path_predictions_celoe, "w") as json_file:
-#         json.dump(celoe_preds, json_file, indent=2)
-
-#     with open(file_path_evaluations_celoe, "w") as json_file:
-#         json.dump(celoe_performance, json_file, indent=2)
-#     file_path_predicitions = f"results/predictions/{dataset}.json"
-#     with open(file_path_predicitions, "w") as json_file:
-#         json.dump(predicition_data, json_file, indent=2)
+    big_df.to_csv(file_path_predictions_df, index=False)
